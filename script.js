ec5ba76589dfce433b8cedf42217b6fc08d1ca1b
Saud Khan <saudahmedk07@gmail.com>
2023-05-27 00:10:04 +0530

    data saving even after reloading added

--- script.js
+++ script.js
@@ -12,10 +12,17 @@ function AddTask(){
         li.appendChild(span);
     }
     inputbox.value= '';
+    savedata()
 }
 listContainer.addEventListener("click", function(e){
-    if(e.target.tagName=== "LI") e.target.classList.toggle("checked");
-    else if(e.target.tagName === "SPAN") e.target.parentElement.remove();
+    if(e.target.tagName=== "LI") {e.target.classList.toggle("checked");savedata()}
+    else if(e.target.tagName === "SPAN"){ e.target.parentElement.remove();savedata()}
 },false);
 
-
+function savedata(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showtask(){
+    listContainer.innerHTML= localStorage.getItem("data");
+}
+showtask()
